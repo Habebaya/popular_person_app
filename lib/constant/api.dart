b207e585162ee4dff6967bbf817e6f64e57e4b89
habebayasser <habebayasser@gmail.com>
2022-10-02 19:48:21 +0200

    add personPERFIX in api.dart

--- lib/constant/api.dart
+++ lib/constant/api.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 class APIConstants {
   static const String baseUrl = 'https://api.themoviedb.org/3';
   static const String apiKey = 'f2b459686afc63ed3a2b26e24d748c29';
+  static const personPERFIX = '/person';
 
 
 
