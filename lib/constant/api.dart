ce820ac1fc8a4f4ff5b97191b9df11293c40bcf0
habebayasser <habebayasser@gmail.com>
2022-10-02 19:44:06 +0200

    add String headerWithoutToken in api.dart

--- lib/constant/api.dart
+++ lib/constant/api.dart
@@ -4,5 +4,11 @@ class APIConstants {
   static const String baseUrl = 'https://api.themoviedb.org/3';
 
 
+  static const headerWithoutToken = {
+    HttpHeaders.contentTypeHeader: 'application/json',
+    HttpHeaders.acceptHeader: 'application/json'
+  };
+
+
 
 }
