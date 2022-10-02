f3ea2a840a94765e65f6abe30e98a1fc0989820e
habebayasser <habebayasser@gmail.com>
2022-10-02 19:48:53 +0200

    add popularPERFIX in api.dart

--- lib/constant/api.dart
+++ lib/constant/api.dart
@@ -4,6 +4,8 @@ class APIConstants {
   static const String baseUrl = 'https://api.themoviedb.org/3';
   static const String apiKey = 'f2b459686afc63ed3a2b26e24d748c29';
   static const personPERFIX = '/person';
+  static const popularPERFIX = '/popular';
+
 
 
 
