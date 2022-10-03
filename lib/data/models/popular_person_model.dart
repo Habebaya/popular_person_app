0519b37067bc5abfae69a5237ba0aed311a2775a
habebayasser <habebayasser@gmail.com>
2022-10-03 13:06:04 +0200

    implement fromJson method in popularperson model

--- lib/data/models/popular_person_model.dart
+++ lib/data/models/popular_person_model.dart
@@ -1,6 +1,15 @@
 class PopularPerson {
   String? id, name, profilePath,knownForDepartment;
 
-  PopularPerson({this.id, this.name, this.profilePath, this.knownForDepartment})
-  ;
+  PopularPerson({this.id, this.name, this.profilePath, this.knownForDepartment});
+
+
+  PopularPerson.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    profilePath = json['profilePath'];
+    knownForDepartment = json ['known_for_department'];
+
+  }
+
 }
