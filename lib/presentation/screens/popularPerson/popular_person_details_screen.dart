896496082edcfac21b2e456d707e206e0af0e5e2
habebayasser <habebayasser@gmail.com>
2022-10-03 20:07:10 +0200

    create PopularPersonDetailsScreen inside popularPerson Folder

---
+++ lib/presentation/screens/popularPerson/popular_person_details_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class PopularPersonDetailsScreen extends StatefulWidget {
+  const PopularPersonDetailsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PopularPersonDetailsScreen> createState() => _PopularPersonDetailsScreenState();
+}
+
+class _PopularPersonDetailsScreenState extends State<PopularPersonDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
