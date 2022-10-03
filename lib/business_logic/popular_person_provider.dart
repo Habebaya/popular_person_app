46d01c0dcad7dbcecdb12f1abcdbe5600301c719
habebayasser <habebayasser@gmail.com>
2022-10-03 21:14:22 +0200

    implement getPopularPersonImage inside Provider

--- lib/business_logic/popular_person_provider.dart
+++ lib/business_logic/popular_person_provider.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
+import 'dart:developer';
 
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart';
+import 'package:task/data/models/popular_person_image_model.dart';
 import 'package:task/data/models/popular_person_model.dart';
 import 'package:task/data/repositories/popular_person_repo.dart';
 
@@ -11,6 +13,7 @@ class PopularPersonProvider with ChangeNotifier {
   final PopularPersonRepository _popularPersonRepository =
       PopularPersonRepository();
   List<PopularPerson> popularPersonList = [];
+  List<PopularPersonImage> images = [];
   late PopularPerson popularPerson;
 
   Future<void> getAllPopularPerson() async {
@@ -18,19 +21,16 @@ class PopularPersonProvider with ChangeNotifier {
       final popularPersonResponse =
           await _popularPersonRepository.getAllPopularPerson();
       if (Validators.responseIsValid(popularPersonResponse)) {
-        print("habrba");
-
         final decoded = jsonDecode(popularPersonResponse.body);
 
         List popularPerson = decoded['results'];
 
         for (var element in popularPerson) {
           popularPersonList.add(PopularPerson.fromJson(element));
-          print(popularPersonList.length);
         }
       }
     } catch (e) {
-      print(e);
+      log(e.toString());
     }
 
     notifyListeners();
@@ -42,14 +42,34 @@ class PopularPersonProvider with ChangeNotifier {
       response = await _popularPersonRepository.getPopularPerson(id);
       if (Validators.responseIsValid(response)) {
         final decoded = jsonDecode(response.body);
-        print(response.body);
 
         final onePopularPerson = decoded;
         popularPerson = PopularPerson.fromJson(onePopularPerson);
         notifyListeners();
       }
     } catch (e) {
-      print(e);
+      log(e.toString());
+    }
+
+    notifyListeners();
+  }
+
+  Future<void> getPopularPersonImage(int id) async {
+    try {
+      final popularPersonImageResponse =
+          await _popularPersonRepository.getPopularPersonImage(id);
+      if (Validators.responseIsValid(popularPersonImageResponse)) {
+        final decoded = jsonDecode(popularPersonImageResponse.body);
+        log("responde ${decoded}");
+
+        List popularPersonImage = decoded['profiles'];
+
+        for (var element in popularPersonImage) {
+          images.add(PopularPersonImage.fromJson(element));
+        }
+      }
+    } catch (e) {
+      log(e.toString());
     }
 
     notifyListeners();
