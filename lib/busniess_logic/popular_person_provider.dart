0ce7504c21e88925dd0e392cb6f4161724ece56e
habebayasser <habebayasser@gmail.com>
2022-10-03 13:29:40 +0200

    implement getAllPopularPerson Method in popular_person_provider.dart

--- lib/busniess_logic/popular_person_provider.dart
+++ lib/busniess_logic/popular_person_provider.dart
@@ -1,20 +1,42 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart';
 import 'package:task/data/models/popular_person_model.dart';
 import 'package:task/data/repositories/popular_person_repo.dart';
 
+import '../utility/validator.dart';
+
 class PopularPersonProvider with ChangeNotifier {
-  final PopularPersonRepository _popularPersonRepository = PopularPersonRepository();
+  final PopularPersonRepository _popularPersonRepository =
+      PopularPersonRepository();
   List<PopularPerson> popularPersonList = [];
 
   Future<void> getAllPopularPerson() async {
-    Response response;
     try {
-      response = await _popularPersonRepository.getAllPopularPerson();
+      final popularPersonResponse =
+          await _popularPersonRepository.getAllPopularPerson();
+      if (Validators.responseIsValid(popularPersonResponse)) {
+        print(popularPersonResponse.body);
+
+        final decoded = jsonDecode(popularPersonResponse.body);
+        List popularPerson = decoded['results'];
+
+        for (var element in popularPerson) {
+          popularPersonList.add(PopularPerson.fromJson(element));
+        }
+      }
     } catch (e) {
       print(e);
     }
 
     notifyListeners();
+  }
+
+  void classifyPopularPerson(Map<String, dynamic> data) {
+    final List allPopularPersonMap = data['results'];
+    popularPersonList = allPopularPersonMap
+        .map((popularPerson) => PopularPerson.fromJson(popularPerson))
+        .toList();
   }
 }
