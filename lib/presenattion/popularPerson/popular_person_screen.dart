f8b8ecdf4444133e647ca555b48151f434f47fa7
habebayasser <habebayasser@gmail.com>
2022-10-03 14:04:57 +0200

    implement gridView in popular_person_screen.dart

--- lib/presenattion/popularPerson/popular_person_screen.dart
+++ lib/presenattion/popularPerson/popular_person_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:task/business_logic/popular_person_provider.dart';
+
 class PopularPersonScreen extends StatefulWidget {
   const PopularPersonScreen({Key? key}) : super(key: key);
 
@@ -7,8 +10,47 @@ class PopularPersonScreen extends StatefulWidget {
 }
 
 class _PopularPersonScreenState extends State<PopularPersonScreen> {
+  late PopularPersonProvider popularPersonProvider;
+  Future? _future;
+
+  @override
+  void initState() {
+    super.initState();
+    popularPersonProvider =
+        Provider.of<PopularPersonProvider>(context, listen: false);
+    _future = popularPersonProvider.getAllPopularPerson();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("PopularPerson"),
+          centerTitle: true,
+        ),
+        body: FutureBuilder(
+            future: _future,
+            builder: (c, s) {
+              if (s.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else {
+                return GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithMaxCrossAxisExtent(
+                            maxCrossAxisExtent: 200,
+                            crossAxisSpacing: 2.0,
+                            mainAxisSpacing: 2.0,
+                            childAspectRatio: 0.5),
+                    shrinkWrap: true,
+                    physics: const ClampingScrollPhysics(),
+                    padding: EdgeInsets.zero,
+                    itemCount: popularPersonProvider.popularPersonList.length,
+                    itemBuilder: (ctx, index) {
+                      return Text(popularPersonProvider
+                          .popularPersonList[index].name
+                          .toString());
+                    });
+              }
+            }));
   }
 }
